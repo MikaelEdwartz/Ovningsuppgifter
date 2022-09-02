b063a405d87d5b317972b949cf938414b513e02c
mikaeledwartz <Mikael.edwartz@gmail.com>
2022-09-02 11:43:56 +0200

    add läslighet

--- src/se/iths/ovningsuppgifter/OvningarEtt.java
+++ src/se/iths/ovningsuppgifter/OvningarEtt.java
@@ -7,12 +7,11 @@ import java.util.Scanner;
 public class OvningarEtt {
     public static void main(String[] args) {
 
-        //greeting();
-        //sumAndProduct();
-        //decimal();
-        //average();
-        //interest();
-
+        greeting();
+        sumAndProduct();
+        decimal();
+        average();
+        interest();
 
 
     }
@@ -32,85 +31,78 @@ public class OvningarEtt {
 
     //Uppgift 2 och 3
 
-    
-    public static void greeting(){
+
+    public static void greeting() {
+        //skapar en skannen
         Scanner scanner = new Scanner(System.in);
+        //printar ut en fråga om namn och sparar det i en variabel
         System.out.println("Vad är ditt namn?");
         String name = scanner.nextLine();
-
+        // printar ut namnet och frågar efter ett tal
         System.out.println("Hej " + name + "!");
         System.out.println("Vänligen skriv in ett tal");
-
+        //sparar siffran och printar ut det
         int numInput = Integer.parseInt(scanner.nextLine());
-
-      
-
         System.out.println(numInput);
 
     }
+
     //Uppgift 4
-    public static void sumAndProduct(){
-        System.out.println("Vänlingen skriv in ett nummer du vill veta summan och produkten av");
+    public static void sumAndProduct() {
+        //frågar efter 2 siffror användaren vill veta summan och produkten av
+        System.out.println("Vänlingen skriv in två nummer du vill veta summan och produkten av");
         Scanner scanner = new Scanner(System.in);
-
+        //sparar de 2 siffrorna i två variabler
         int number = scanner.nextInt();
-
-        System.out.println("Summan är " + (number + number));
-        System.out.println("Produkten är " + (number * number));
+        int number2 = scanner.nextInt();
+        //gör uträkningarna samt printar ut svaren
+        System.out.println("Summan är " + (number + number2));
+        System.out.println("Produkten är " + number * number2);
 
 
     }
-    //Uppgift 5
-    public static void decimal(){
 
+    //Uppgift 5
+    public static void decimal() {
+        //frågar användaren efter ett decimaltal
         System.out.println("Skriv in ett decimaltal, (använd komma.)");
         Scanner scanner = new Scanner(System.in);
-        //
+        // sparar siffran i en variabel och omvandlar sedan till en int, samt printar ut
         double number = scanner.nextDouble();
         int doubleToInt = (int) number;
         System.out.println(doubleToInt);
 
 
     }
+
     //Uppgift 6
-    public static void average(){
+    public static void average() {
+        //frågar användaren efter 2 olika siffror
         Scanner scanner = new Scanner(System.in);
         System.out.println("Vad god skriv in 2 olika siffror");
-
+        // sparar inputen i två variabler
         int number1 = scanner.nextInt();
         int number2 = scanner.nextInt();
-
+        //räknar ut vad medelvärdet av de två talen var samt printar ut
         System.out.println((double) (number1 + number2) / 2);
     }
+
     //Uppgift 7
-    public static void interest(){
+    public static void interest() {
+        //frågar hur mycket pengar som finns på kontot och sparar det i en variabel
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hur mycket pengar har du på ditt konto?");
-
         double moneyOnAccount = scanner.nextDouble();
+        //frågar användaren hur mycket räntan är
         System.out.println("Vad är räntan? Svara i heltal (t.ex 2 för 2%)");
-
-        double interest = scanner.nextDouble();
-        interest = (interest / 100) + 1;
-
+        double inputInterest = scanner.nextDouble();
+        //omvandlar inputen till rätt format för att räkna ut räntan samt räknar ut vad det är efter räntan
+        double interest = (inputInterest / 100) + 1;
         double moneyAfterInterest = moneyOnAccount * (1 + interest);
+        //printar ut svaret
         System.out.println(moneyAfterInterest + " kr");
 
     }
 
 
-    /*
-    D
-    D
-
-    D
-    D
-    D
-    D
-    D
-    D
-
-    D
-    D
-     */
 }
--- src/se/iths/ovningsuppgifter/OvningarTvå.java
+++ src/se/iths/ovningsuppgifter/OvningarTvå.java
@@ -9,48 +9,48 @@ public class OvningarTvå {
 
         Scanner scanner = new Scanner(System.in);
 
-        //   exercise3();
-        //   exercise4(scanner);
-        //   exercise5(scanner);
-        //   exercise6(scanner);
-        //   exercise7();
-        //   exercise8();
-        //   exercise9();
-        //   exercise10(scanner);
-        //   exercise11();
+        exercise3(scanner);
+        exercise4(scanner);
+        exercise5(scanner);
+        exercise6(scanner);
+        exercise7();
+        exercise8();
+        exercise9();
+        exercise10(scanner);
+        exercise11();
 
     }
 
 
-    private static void exercise3() {
-        Console c = System.console();
-        System.out.println("Hitta på ett lösenord");
+    private static void exercise3(Scanner scanner) {
+        //skapar ett lösenord
         String password = "password";
 
         while (true) {
+            //ber användaren skriva in lösenord, gissar användaren rätt går vi ut ur loopen
             System.out.println("Skriv in ditt lösenord");
-            String input = "";
-
-            input = c.readLine();
+            String input = scanner.next();
 
             if (input.equals(password))
                 break;
-
+            //printar ut fel om lösenordet inte stämmer
+            System.out.println("Fel lösenord");
         }
-        System.out.println("Fel lösenord");
-
 
     }
 
 
     private static void exercise4(Scanner scanner) {
+        //frågar användaren efter ett tal i en loop som körs tills break kommnadot nås
         while (true) {
             int biggerThan100 = scanner.nextInt();
-
+            //är input högre än 100 printas det ut
             if (biggerThan100 > 100)
                 System.out.println("Talet är större än 100");
+                //är input mindre än 100 printas det ut
             else if (biggerThan100 < 100)
                 System.out.println("Talet är mindre än 100");
+                //är input 100 printas det ut och loopen avbryts
             else {
                 System.out.println("Talet är 100!");
                 break;
@@ -61,8 +61,9 @@ public class OvningarTvå {
     }
 
     private static void exercise5(Scanner scanner) {
+        //frågar användaren efter en månad
         String input = scanner.next();
-
+        //printar ut nummervärdet av månaden som skrivs in
         switch (input) {
             case "januari" -> System.out.println(1);
             case "februari" -> System.out.println(2);
@@ -81,41 +82,55 @@ public class OvningarTvå {
     }
 
     private static void exercise6(Scanner scanner) {
-        int randomNumber = (int) Math.random() * 100 + 1;
-
+        //skapar ett random värde mellan 1 och 100
+        int randomNumber = (int) (Math.random() * 100) + 1;
+        //skapar en variabel som håller koll på antalet gissningar
+        int numberOfGuesses = 0;
+        //skapar en loop som beroende på input printar ut om gissningen är för låg eller för hög
         while (true) {
+
             int input = scanner.nextInt();
+            //lägger till antalet gissningar
+            numberOfGuesses++;
+            //är gissningen högre än det slumpade numret printas det ut
             if (input > randomNumber) {
                 System.out.println("Din gissning är för hög.");
+                //är gissningen mindre än det slumpade numret printas det ut
             } else if (input < randomNumber) {
                 System.out.println("Din gissning är för låg!");
+                //är gissningen inte några av de ovanstående måste det ha blivit rätt och printas ut. Samt går ut ur loopen
             } else {
                 System.out.println("Rätt!");
                 break;
             }
         }
+        System.out.println("Du lyckades på " + numberOfGuesses + " gissningar");
     }
 
 
     //Uppgift 7
     private static void exercise7() {
+        //skapar en for loop som kör 16 gånger och printar ut i varje gång loppen körs (printar ut 1-16)
         for (int i = 1; i <= 16; i++) {
             System.out.println(i);
-
 
         }
     }
 
 
     private static void exercise8() {
+        //en for loop som körs 20 gånger och varje gång talet modulo 2 är lika med 0 printas talet ut.
         for (int i = 20; i >= 2; i--) {
             if (i % 2 == 0)
                 System.out.println(i);
         }
     }
 
+
     private static void exercise9() {
+        //initialerar ett tal
         int number = 65536;
+        //medans talet är större än 2 printas det ut och divideras med 2
         while (number > 2) {
             System.out.println(number);
             number /= 2;
@@ -125,42 +140,39 @@ public class OvningarTvå {
 
 
     private static void exercise10(Scanner scanner) {
-        String input = "";
+        //initialerar fullständiga strängen
         String completeInput = "";
+        //skapar en loop
         while (true) {
+            //frågar användaren efter ett ord och sparar det i en variabel
             System.out.println("Skriv in ett ord");
-            input = scanner.nextLine();
-
+            String input = scanner.nextLine();
+            //om inputen är tom eller består av en punkt avbryts loopen och det fullständiga ordet printas ut
             if (input.equals("") || input.equals("."))
                 break;
+            //lägger ihop den fullständiga strängen med inputen
             completeInput = completeInput + input + " ";
         }
+        //printar ut fullstänga strängen
         System.out.println(completeInput);
     }
 
 
     private static void exercise11() {
+        //väljer ett tal som ska räknas ut kommer alltid köras tills att input är mindre än 2
         int input = 3;
         while (input > 2) {
-
+            // om inputen modulo 2 är lika med 0 kommer inpute delas
             if (input % 2 == 0)
                 input = input / 2;
+                // om inputen modulo 2 är lika med 1 kommer inputen multipleras med 3 och adderas med 1
             else
                 input = input * 3 + 1;
-
+            //printar ut talet innan loopen startas om
             System.out.println(input);
         }
     }
 
 }
 
-/*¨
- *
- *
- *
- *
- *
- *
- *
- * */
 
