b666acbd65eefc84a7101fa29d6a5c2478fd3f80
mikaeledwartz <Mikael.edwartz@gmail.com>
2022-09-07 13:55:58 +0200

    add complete KapitelTre

--- src/se/iths/ovningsuppgifter/KapitelTre.java
+++ src/se/iths/ovningsuppgifter/KapitelTre.java
@@ -1,4 +1,26 @@
 package se.iths.ovningsuppgifter;
 
+import java.util.Scanner;
+
 public class KapitelTre {
+
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        double convert = 0;
+        try {
+        convert = Double.parseDouble(scanner.nextLine());
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println(convert);
+    }
+
+
+
+
 }
+
+
