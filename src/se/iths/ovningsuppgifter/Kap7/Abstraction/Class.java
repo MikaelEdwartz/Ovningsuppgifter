11a4602d2895f74f5c0ae01255e5f5d11025335f
mikaeledwartz <Mikael.edwartz@gmail.com>
2022-09-21 12:31:41 +0200

    Add add exercise9

---
+++ src/se/iths/ovningsuppgifter/Kap7/Abstraction/Class.java
@@ -0,0 +1,9 @@
+package se.iths.ovningsuppgifter.Kap7.Abstraction;
+
+
+public abstract class Class {
+
+    public abstract void print();
+
+
+}
---
+++ src/se/iths/ovningsuppgifter/Kap7/Abstraction/ClassExtend.java
@@ -0,0 +1,12 @@
+package se.iths.ovningsuppgifter.Kap7.Abstraction;
+
+import java.sql.SQLOutput;
+
+public class ClassExtend extends Class{
+
+
+    @Override
+    public void print() {
+        System.out.println("");
+    }
+}
